45ef0e81f0603b45ea3f8521956b3ab97c042952
Dan Watkins <dwatkins@valpineware.com>
2014-04-12 16:04:54 -0500

    Cleaned out Hierarchy demo code

--- Src/OpenGLStudy.cpp
+++ Src/OpenGLStudy.cpp
@@ -7,12 +7,9 @@
 
 #include "OpenGLStudy.h"
 #include "./Core/Core.h"
-#include "Hierarchy.h"
 
 namespace glz
 {	
-	Hierarchy hierarchy;
-
 	Float calcFrustumScale(Float fovDeg)
 	{
 		Float fovRad = degToRad(fovDeg);
@@ -31,7 +28,6 @@ namespace glz
 	}
 
 
-
 	Float computeAngleRad(Float elapsedTime, Float loopDuration)
 	{
 		Float scale = TWO_PI / loopDuration;
@@ -41,72 +37,18 @@ namespace glz
 	}
 
 
-
-
-	const Float frustumScale = calcFrustumScale(45.0f);
-
 	void OpenGLStudy::initGlProgram()
 	{
-		//load shaders
-		Uint shaders[2];
-		shaders[0] = Shader::loadShader(String(gDefaultPathShaders) + "standard.vert", GL_VERTEX_SHADER);
-		shaders[1] = Shader::loadShader(String(gDefaultPathShaders) + "standard.frag", GL_FRAGMENT_SHADER);
 
-		mProgram = Shader::linkFromShaders(shaders, 2);
-
-		
-		//configure view matrices
-		modelToCameraUnif = glGetUniformLocation(mProgram, "modelToCameraMatrix");
-		cameraToClipMatrixUnif = glGetUniformLocation(mProgram, "cameraToClipMatrix");
-
-		Float zNear = 1.0f;
-		Float zFar = 61.0f;
-
-
-		cameraToClipMatrix[0].x = frustumScale;
-		cameraToClipMatrix[1].y = frustumScale;
-		cameraToClipMatrix[2].z = (zFar+zNear) / (zNear-zFar);
-		cameraToClipMatrix[2].w = -1.0f;
-		cameraToClipMatrix[3].z = (2*zFar*zNear) / (zNear-zFar);
-
-		glUseProgram(mProgram);
-		glUniformMatrix4fv(cameraToClipMatrixUnif, 1, GL_FALSE, glm::value_ptr(cameraToClipMatrix));
-		glUseProgram(0);
 	}
 
 
 	void OpenGLStudy::initVertexBuffer()
 	{
-		glGenBuffers(1, &vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-		glGenBuffers(1, &ibo);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexData), indexData, GL_STATIC_DRAW);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-
-		size_t colorDataOffset = sizeof(Float)* 3 * numberOfVertices;
-		glBindBuffer(GL_ARRAY_BUFFER, vao);
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)colorDataOffset);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-
-		glBindVertexArray(0);
+		
 	}
 
 
-
-
-	/*=================================================================================================
-	   OpenGLWindow Specific
-	=================================================================================================*/
 	OpenGLStudy::OpenGLStudy() : cameraToClipMatrix(0.0f)
 	{
 	}
@@ -114,20 +56,8 @@ namespace glz
 	
 	void OpenGLStudy::onStartup()
 	{
-		sf::Clock seedTime;
-		srand((Uint)seedTime.getElapsedTime().asMicroseconds());
-
 		initGlProgram();
 		initVertexBuffer();
-
-		glEnable(GL_CULL_FACE);
-		glCullFace(GL_BACK);
-		glFrontFace(GL_CW);
-
-		glEnable(GL_DEPTH_TEST);
-		glDepthMask(GL_TRUE);
-		glDepthFunc(GL_LEQUAL);
-		glDepthRange(0.0f, 1.0f);
 	}
 
 	
@@ -137,12 +67,6 @@ namespace glz
 		static const Float clearColor[] = { 1.0f, 1.0f, 0.9f, 1.0f };
 		glClearBufferfv(GL_COLOR, 0, clearColor);
 
-		glClearColor(1.0f, 1.0f, 0.9f, 1.0f);
-		glClearDepth(1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		RenderData rd = { mProgram, vao, modelToCameraUnif };
-		hierarchy.draw(rd);
 	}
 
 
@@ -165,15 +89,6 @@ namespace glz
 
 				break;
 			}
-
-			case GLFW_KEY_A: hierarchy.adjustBase(true); break;
-			case GLFW_KEY_D: hierarchy.adjustBase(false); break;
-
-			case GLFW_KEY_W: hierarchy.adjustMainArm(false); break;
-			case GLFW_KEY_S: hierarchy.adjustMainArm(true); break;
-
-			case GLFW_KEY_R: hierarchy.adjustLowerArm(false); break;
-			case GLFW_KEY_F: hierarchy.adjustLowerArm(true); break;
 		}
 	}
 
