43c2c4d3ede8b2cdc0db9801476a206816590830
Dan Watkins <dwatkins@valpineware.com>
2014-07-31 20:53:51 -0500

    Imported and tweaked the KTX loader from SB6

---
+++ Src/ktx.h
@@ -0,0 +1,47 @@
+//This file came from the SB6 example code
+#ifndef KTX_H
+#define KTX_H
+
+namespace sb6
+{
+
+namespace ktx
+{
+
+namespace file
+{
+
+struct header
+{
+	unsigned char       identifier[12];
+	unsigned int        endianness;
+	unsigned int        gltype;
+	unsigned int        gltypesize;
+	unsigned int        glformat;
+	unsigned int        glinternalformat;
+	unsigned int        glbaseinternalformat;
+	unsigned int        pixelwidth;
+	unsigned int        pixelheight;
+	unsigned int        pixeldepth;
+	unsigned int        arrayelements;
+	unsigned int        faces;
+	unsigned int        miplevels;
+	unsigned int        keypairbytes;
+};
+
+union keyvaluepair
+{
+	unsigned int        size;
+	unsigned char       rawbytes[4];
+};
+
+unsigned int load(const char * filename, unsigned int tex = 0);
+bool save(const char * filename, unsigned int target, unsigned int tex);
+
+}
+
+}
+
+}
+
+#endif
